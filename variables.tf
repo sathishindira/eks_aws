9b2a8874a2ff509ca92afb28c1966b681d0ed83c
sathish <sathishcitmech@gmail.com>
2025-12-09 06:00:59 +0000

    updated eks cluster version to 1.34

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.28"
+  default     = "1.34"
 }
 
 variable "vpc_cidr" {
